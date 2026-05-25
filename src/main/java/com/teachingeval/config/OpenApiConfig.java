1737bc4359faaaacf88ad334b731fd07663e74ed
LaT-SKY <miprota@163.com>
2026-05-25 14:21:50 +0800

    修改 deploy.yml - 修复一些问题

--- src/main/java/com/teachingeval/config/OpenApiConfig.java
+++ src/main/java/com/teachingeval/config/OpenApiConfig.java
@@ -2,18 +2,26 @@ package com.teachingeval.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+
     @Bean
     public OpenAPI teachingEvaluationOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("教学评价系统 API")
                         .description("教师端作品评价系统接口文档")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addServersItem(new Server().url("/").description("Default Server URL"));
     }
 }
-
