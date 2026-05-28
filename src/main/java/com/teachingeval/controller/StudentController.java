764922ad92783d9f2965ed187b4431dc064240f8
LaT-SKY <liujunzhe070611@gmail.com>
2026-05-28 17:29:10 +0800

    重新提交

--- src/main/java/com/teachingeval/controller/StudentController.java
+++ src/main/java/com/teachingeval/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.teachingeval.controller;
 
+import com.teachingeval.dto.StudentRequest;
 import com.teachingeval.entity.Student;
 import com.teachingeval.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,11 +35,11 @@ public class StudentController {
         return studentService.listStudents();
     }
 
-    @Operation(summary = "新增学生", description = "录入学生学号、姓名和班级信息，学号不能重复。")
+    @Operation(summary = "新增学生", description = "录入学生学号、姓名和班级信息，学号不能为空且不能重复。")
     @PostMapping("/students")
     @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student) {
-        return studentService.createStudent(student);
+    public Student createStudent(@Valid @RequestBody StudentRequest request) {
+        return studentService.createStudent(request);
     }
 
     @Operation(summary = "删除学生", description = "根据学生主键 ID 删除指定学生。")
