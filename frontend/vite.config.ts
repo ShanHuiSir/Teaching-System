fa3e11716b22339b322cbd252416eb5584e70a79
LaT-SKY <miprota@163.com>
2026-06-21 17:19:13 +0800

    feat: 图像和视频预览 - 可以预览图像了 - 可以预览视频了

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -55,6 +55,9 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    headers: {
+      'Cache-Control': 'no-store',
+    },
     proxy: {
       '/ai': {
         target: 'http://localhost:8000',
